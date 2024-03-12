d7fa4dfa4e9f5c91f1bf0dcd70482eb4c3fc80df
adilrion <adilmahmoudrion@gmail.com>
2024-03-12 06:17:29 +0600

    restart

--- C/Practice/Pointer.c
+++ C/Practice/Pointer.c
@@ -2,10 +2,11 @@
 int main(){
     int checkAddress= 30;
     printf("Address of checkAddress variable: %p", &checkAddress);
+    
 
     /* Create pointer variable */
-    int* pointer = &checkAddress;
-    printf("\nAddress of new pointer variable: %p", pointer);
+    int *pointer = &checkAddress;
+    printf("\nAddress of new pointer variable: %d", *pointer);
     printf("\nvalue of new pointer variable: %p", checkAddress);
     return 0;
 }
