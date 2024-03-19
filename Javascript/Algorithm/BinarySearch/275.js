7f7295dc6e6eb75e3a519f83bda9f5ed910fcda2
adilrion <adilmahmoudrion@gmail.com>
2024-03-20 01:30:33 +0600

    find H-Index II

---
+++ Javascript/Algorithm/BinarySearch/275.js
@@ -0,0 +1,16 @@
+// 275. H-Index II
+
+var hIndex = function (citations) {
+    citations.sort((a, b) => b - a);
+    let hIndex = 0;
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] >= i + 1) {
+            hIndex = i + 1;
+        } else {
+            break;
+        }
+    }
+    return hIndex;
+};
+
+console.log(hIndex([0, 1, 3, 5, 6, 7]));
