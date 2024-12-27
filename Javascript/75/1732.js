a71432e7eef70e2acd4fd8592aab7e29e61a5175
adilrion <adilmahmoudrion@gmail.com>
2024-12-28 01:34:27 +0600

    724. Find Pivot Index

--- Javascript/75/1732.js
+++ Javascript/75/1732.js
@@ -1,13 +1,8 @@
 // 1732. Find the Highest Altitude
-
 var largestAltitude = function (gain) {
-
-
   if (gain.length === 0) {
     return 0;
   }
-  
-
   let max = 0;
   let point = 0;
   for (let i = 0; i < gain.length; i++) {
@@ -16,12 +11,7 @@ var largestAltitude = function (gain) {
     max = Math.max(max, point)
     
   }
-  
-  
   return max
-  
 };
-
-
 let gain = [-5, 1, 5, 0, -7]
 console.log(largestAltitude(gain));
