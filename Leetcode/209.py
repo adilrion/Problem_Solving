cfaaf869284dbdbeb54507e838aca96deb2f4a2d
adilrion <adilmahmoudrion@gmail.com>
2025-05-13 23:58:40 +0600

    209

---
+++ Leetcode/209.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minSubArray(self, target: int, numbers: list[int]) -> int:
+        minL = float("inf")
+        sum = 0
+        left = 0
+
+        for right in range(len(numbers)):
+            sum += numbers[right]
+            while sum >= target:
+                minL = min(minL, right - left + 1)
+                sum -= numbers[left]
+                left += 1
+
+        return 0 if minL == 0 else minL
+
+
+target = 7
+nums = [2, 3, 1, 2, 4, 3]
+print(Solution().minSubArray(target, nums))
